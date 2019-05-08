f02666da923a82a6adc91247710f6d3074d974ac
maxwhalezhou <maxwell.chi.zhou@gmail.com>
2019-05-08 14:24:27 -0700

    Client updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
 
 
 
@@ -91,27 +95,47 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TextField
-          id="string-find"
-          label="Text to Replace"
-          value={this.state.stringFind}
-          onChange={this.handleStringFind}
-          margin="normal"
-        />
-        <TextField
-          id="string-replace"
-          label="Replacement Text"
-          value={this.state.StringReplace}
-          onChange={this.handleStringReplace}
-          margin="normal"
-        />
-        <Button onClick={this.handleSetProxy} variant="contained">
-            Set Proxy
-        </Button>
+        <Card style={{margin:'10px', backgroundSize:'480px 360px, contain', backgroundImage:'url("https://media.giphy.com/media/3oxHQk2aTC2bCYxoRi/giphy.gif")'}}>
+        <CardContent>
+          <Typography variant="h3" >
+            Find and Replace Proxy
+         </Typography>
+          <Typography variant="body1">
+            Enter the value you would like to replace below. Then, hit 'Set Proxy' to initialize the proxy. If you would like to replace a different set of values, hit 'Reset' to reset the service and repeat the first step.
+         </Typography>
+          <div>
+            <TextField
+              id="string-find"
+              label="Text to Replace"
+              value={this.state.stringFind}
+              onChange={this.handleStringFind}
+              margin="normal"
+            />
+          </div>
 
-          <Button onClick={this.handleResetProxy} variant="contained">
-            Reset
+          <div >
+            <TextField
+              id="string-replace"
+              label="Replacement Text"
+              value={this.state.StringReplace}
+              onChange={this.handleStringReplace}
+              margin="normal"
+            />
+          </div>
+          </CardContent>
+          <div className="button-holder" style={{textAlign: 'center'}}>
+            <div>
+              <Button onClick={this.handleSetProxy} variant="contained" style={{ margin: '10px' }}>
+                Set Proxy
         </Button>
+              <Button onClick={this.handleResetProxy} color="secondary" variant="contained" >
+                Reset
+        </Button>
+            </div>
+          </div>
+
+          </Card>
+
       </div>
     );
   }
